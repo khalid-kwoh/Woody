47a174e1b2dd961812958a16d5af01e612b346b1
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-20 17:56:39 +0900

    refactor: Delete validation check & renaming
    JWTs perform validation on their own

--- database.js
+++ database.js
@@ -28,24 +28,17 @@ const updateDomain = (id, domain, apiKey, apiToken) => {
   });
 };
 
-const updateAPIToken = (sessionKey, newApiToken) => {
+const updateAPIToken = (domain, newApiToken) => {
   return new Promise((resolve, reject) => {
-    // sessionKey가 유효한지 확인
-    const [sessionValue, expirationTime] = sessionKey.split('.');
-    if (!sessionValue || !expirationTime || Date.now() > parseInt(expirationTime) + 30 * 60 * 1000) {
-      reject(new Error('session_key is invalid or has expired'));
-    }
-
     // domains 테이블에서 session_key가 일치하는지 확인
-    const query = 'SELECT * FROM domains WHERE session_key = ?';
-    connection.query(query, [sessionKey], (err, results) => {
+    const query = `SELECT * FROM domains WHERE domain = '${ domain }'`;
+    connection.query(query, [domain], (err, results) => {
       if (err) {
         reject(err);
       } else {
         if (results.length === 0) {
           reject('session_key does not match any record in domains table');
         } else {
-          const domain = results[0].domain;
           const updateQuery = 'UPDATE domains SET api_token = ? WHERE domain = ?';
           connection.query(updateQuery, [newApiToken, domain], (err, result) => {
             if (err) {
