07c1258e68d6a26c911f94e1a14809751c89eefc
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-20 14:27:15 +0900

    feat: Create random session key

---
+++ session.js
@@ -0,0 +1,1 @@
+function createSessionKey() {    const expirationTime = Date.now() + 30 * 60 * 1000; // 30분 후의 시간    const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);    const sessionKey = `${randomString}.${expirationTime}`;    return sessionKey;}module.exports = {    createSessionKey};
